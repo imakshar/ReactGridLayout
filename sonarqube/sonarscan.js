cc486b3ca30f57c5161bfb6b2eb1221622ff7601
Akshar <aksharsarvaiya123@gmail.com>
2023-02-03 22:33:34 +0530

    sonar project description updated

--- sonarqube/sonarscan.js
+++ sonarqube/sonarscan.js
@@ -6,7 +6,7 @@ scanner(
     token: "e838dd064de0e78c5c39bcbb23aeb758353944ca",
     options: {
       "sonar.projectName": "react-grid-layout",
-      "sonar.projectDescription": "Here I can add a description of my project",
+      "sonar.projectDescription": "React Grid Layout Demo",
       "sonar.projectKey": "7abd899dee8cc7a55aeac420a38f9e431e0b2aa0",
       "sonar.projectVersion": "0.0.1",
       "sonar.exclusions": "",
